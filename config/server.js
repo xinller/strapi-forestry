fbe3f548600168947e0f18bd0dd71f1de7912c12
xinller <xl1012796466@gmail.com>
2021-01-06 16:17:30 +0800

    port

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
+    port: env.int('PORT', 1338),
     admin: {
         auth: {
             secret: env('ADMIN_JWT_SECRET', '94d29d19ab615895731865ead66ac04f'),
         },
     },
-    url: env('', 'http://localhost:1337'),
+    url: env('', 'http://localhost:1338'),
 });
